3e57d20e1d34f78b35eee33095bf63726a663377
Jose Gregorio Rincon <josegregoriorincon@Joses-MacBook-Pro.local>
2021-01-26 06:27:23 -0500

    01 Bases

---
+++ leaflet/node_modules/@asymmetrik/ngx-leaflet-markercluster/src/leaflet-markercluster/leaflet-markercluster.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { LeafletMarkerClusterDirective } from './leaflet-markercluster.directive';
+
+@NgModule({
+	exports: [ LeafletMarkerClusterDirective ],
+	declarations: [ LeafletMarkerClusterDirective ]
+})
+export class LeafletMarkerClusterModule {
+
+}
