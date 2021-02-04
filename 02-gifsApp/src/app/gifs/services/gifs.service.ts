8e1ea1b18917fd58a5cc37ec035f6f11c3eb45bc
José Gregorio Rincón <jgregoriorincon@gmail.com>
2021-02-04 04:55:29 -0500

    Add LocalStorage

--- 02-gifsApp/src/app/gifs/services/gifs.service.ts
+++ 02-gifsApp/src/app/gifs/services/gifs.service.ts
@@ -17,18 +17,25 @@ export class GifsService {
     return [...this._historial];
   }
 
-  constructor(private http:HttpClient) { }
+  constructor(private http:HttpClient) {
+    this._historial = JSON.parse(localStorage.getItem('historialGifsApp')!) || [];
+    this.resultados = JSON.parse(localStorage.getItem('resultadosGifsApp')!) || [];
+  }
 
   buscarGifs(query:string = '') {
     query = query.trim().toLowerCase();
     if (!this._historial.includes(query)) {
       this._historial.unshift(query);
       this._historial = this._historial.slice(0,10);
+
+      localStorage.setItem('historialGifsApp', JSON.stringify(this._historial));
     }
+
     this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=${this.apiKey}&q=${query}&limit=10&offset=0&rating=g&lang=en`)
       .subscribe((response) => {
         console.log(response.data);
         this.resultados = response.data;
+        localStorage.setItem('resultadosGifsApp', JSON.stringify(this.resultados));
       })
 
   }
