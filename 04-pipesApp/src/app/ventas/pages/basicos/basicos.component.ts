a9bd5a853c476814139b7ee06f8fbac7dc5bd43a
José Gregorio Rincón <jgregoriorincon@gmail.com>
2021-03-24 06:54:01 -0500

    Mapas

--- 04-pipesApp/src/app/ventas/pages/basicos/basicos.component.ts
+++ 04-pipesApp/src/app/ventas/pages/basicos/basicos.component.ts
@@ -6,11 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BasicosComponent implements OnInit {
+export class BasicosComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  nombreLower: string = 'josé';
+  nombreUpper: string = 'JOSÉ';
+  nombreCompleto: string = 'josé gRegoRio Rincón albaRRacín';
 
 }
