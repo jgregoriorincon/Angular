9c9658540e4da42e380455a39d6bf9a81ccf3261
José Gregorio Rincón <jgregoriorincon@gmail.com>
2021-04-07 06:46:12 -0500

    Add Components

---
+++ 05-heroesApp/src/app/heroes/heroes.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AddComponent } from './pages/add/add.component';
+import { FindComponent } from './pages/find/find.component';
+import { HeroComponent } from './pages/hero/hero.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ListComponent } from './pages/list/list.component';
+
+@NgModule({
+  declarations: [
+    AddComponent,
+    FindComponent,
+    HeroComponent,
+    HomeComponent,
+    ListComponent,
+  ],
+  imports: [CommonModule],
+})
+export class HeroesModule {}
---
+++ 05-heroesApp/src/app/heroes/pages/find/find.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-find',
+  templateUrl: './find.component.html',
+  styles: [
+  ]
+})
+export class FindComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
