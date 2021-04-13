f27fdd5b7d822c3049910acda259b6eb53fa6c3a
Jose Gregorio Rincon <jgregoriorincon@gmail.com>
2021-04-13 16:56:00 -0500

    Add Hero

--- 05-heroesApp/src/app/heroes/pages/add/add.component.ts
+++ 05-heroesApp/src/app/heroes/pages/add/add.component.ts
@@ -1,16 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { Heroe, Publisher } from '../../interfaces/heroe.interface';
 
 @Component({
   selector: 'app-add',
   templateUrl: './add.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class AddComponent implements OnInit {
+  publishers = [
+    {
+      id: 'DC Comics',
+      desc: 'DC',
+    },
+    {
+      id: 'Marvel Comics',
+      desc: 'Marvel',
+    },
+  ];
 
-  constructor() { }
+  heroe: Heroe = {
+    superhero: '',
+    publisher: Publisher.DCComics,
+    alter_ego: '',
+    first_appearance: '',
+    characters: '',
+    alt_img: '',
+  };
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  guardar() {
+    console.log(this.heroe);
   }
-
 }
--- 05-heroesApp/src/app/material/material.module.ts
+++ 05-heroesApp/src/app/material/material.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 
-import {MatAutocompleteModule} from '@angular/material/autocomplete';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatIconModule} from '@angular/material/icon';
-import {MatInputModule} from '@angular/material/input';
-import {MatListModule} from '@angular/material/list';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 
 @NgModule({
@@ -25,6 +26,7 @@ import {MatToolbarModule} from '@angular/material/toolbar';
     MatInputModule,
     MatListModule,
     MatProgressSpinnerModule,
+    MatSelectModule,
     MatSidenavModule,
     MatToolbarModule
   ]
