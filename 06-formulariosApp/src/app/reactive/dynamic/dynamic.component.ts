3ee6f438dbad7993261b04dd7c3c041d8e050784
Jose Gregorio Rincon <jgregoriorincon@gmail.com>
2021-04-25 19:40:19 -0500

    Dynamic Form Reactive

--- 06-formulariosApp/src/app/reactive/dynamic/dynamic.component.ts
+++ 06-formulariosApp/src/app/reactive/dynamic/dynamic.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  FormArray,
+  FormControl,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-dynamics',
@@ -6,7 +13,55 @@ import { Component, OnInit } from '@angular/core';
   styles: [],
 })
 export class DynamicComponent implements OnInit {
-  constructor() {}
+  personForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    favorites: this.fb.array(
+      [
+        this.fb.control('Test 1', [Validators.required]),
+        this.fb.control('Test 2', [Validators.required]),
+      ],
+      [Validators.required]
+    ),
+  });
+
+  newFavorite: FormControl = this.fb.control('', [Validators.required]);
+
+  public get favoritesArr() {
+    return this.personForm.get('favorites') as FormArray;
+  }
+
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {}
+
+  validateControl(control: string) {
+    return (
+      this.personForm.controls[control].errors &&
+      this.personForm.controls[control].touched
+    );
+  }
+
+  addFavorite() {
+    if (this.newFavorite.invalid) {
+      return;
+    }
+
+    this.favoritesArr.push(
+      this.fb.control(this.newFavorite.value, [Validators.required])
+    );
+
+    this.newFavorite.reset();
+  }
+
+  deleteFavorite(index: number) {
+    this.favoritesArr.removeAt(index);
+  }
+
+  savePerson() {
+    if (this.personForm.invalid) {
+      this.personForm.markAllAsTouched();
+      return;
+    }
+    console.log(this.personForm.value);
+  }
 }
