131f64fb66a1ef8d3f8b0765d1a3bf5d95816f48
Jose Gregorio Rincon <jgregoriorincon@gmail.com>
2021-05-13 00:34:25 -0500

    Interval Hook

--- 08-lifecycle/src/app/components/muestra-nombre/muestra-nombre.component.ts
+++ 08-lifecycle/src/app/components/muestra-nombre/muestra-nombre.component.ts
@@ -8,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MuestraNombreComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    console.log('constructor');
+  }
 
   ngOnInit(): void {
+    console.log('ngOnInit');
   }
 
 }
--- 08-lifecycle/src/app/pages/page-one/page-one.component.ts
+++ 08-lifecycle/src/app/pages/page-one/page-one.component.ts
@@ -1,16 +1,67 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  OnChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy,
+} from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-page-one',
   templateUrl: './page-one.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class PageOneComponent implements OnInit {
+export class PageOneComponent
+  implements
+    OnInit,
+    OnChanges,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked,
+    OnDestroy {
+  nombre: string = 'Gregorio';
+  segundos: number = 0;
+  timerSuscription!: Subscription;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    console.log('constructor');
+  }
+  ngOnChanges(changes: import('@angular/core').SimpleChanges): void {
+    console.log('ngOnChanges');
+  }
+  ngDoCheck(): void {
+    console.log('ngDoCheck');
+  }
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit');
+  }
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked');
+  }
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit');
+  }
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked');
   }
 
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy');
+    this.timerSuscription.unsubscribe();
+    console.log('Timer limpiado;');
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit');
+    this.timerSuscription = interval(1000).subscribe((i) => {
+      this.segundos = i;
+    });
+  }
 }
